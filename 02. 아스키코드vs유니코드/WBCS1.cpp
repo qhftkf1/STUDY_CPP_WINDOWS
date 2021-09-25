929cf2bf06f4b452791557b9171a510809c26ae2
Evan Song <qhftkf1@naver.com>
2021-09-25 17:32:14 +0900

    2021.09.25 아스키 코드 vs 유니 코드

---
+++ 02. 아스키코드vs유니코드/WBCS1.cpp
@@ -0,0 +1,16 @@
+/*
+	WBCS1.cpp
+*/
+
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main(void)
+//{
+//	wchar_t str[] = L"ABC";
+//	int size = sizeof(str);
+//	int len = wcslen(str);
+//
+//	printf("배열의 크기: %d \n", size);
+//	printf("문자열의 길이: %d \n", len);
+//}
